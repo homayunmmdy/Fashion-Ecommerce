44c885bc2aa7f848387075ea17fa87e01cb7fe23
Homayoun <homayoun763@gmail.com>
2024-04-06 02:31:05 +0330

    Footer

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import SiteConfig from "@/app/[locale]/config/site"
 import { useLocale } from "next-intl";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,17 +12,18 @@ export const metadata = {
   description: SiteConfig.description,
 };
 
-export default function RootLayout({ children , params }) {
+export default function RootLayout({ children, params }) {
   const locale = useLocale();
 
   if (params.locale !== locale) {
     notFound();
   }
   return (
-    <html lang={locale}  dir={locale === 'fa' ? 'rtl' : 'ltr'}>
+    <html lang={locale} dir={locale === 'fa' ? 'rtl' : 'ltr'}>
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
