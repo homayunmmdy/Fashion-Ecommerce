3035f2fea26ab2e8ff3536101b94f34ba1eae209
Homayoun <homayoun763@gmail.com>
2024-04-06 02:43:05 +0330

    Category

---
+++ app/[locale]/components/Categories.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const Categories = () => {
+    return (
+        <div>
+            <div className="columns-5 gap-0 pt-40">
+                <figure className="blog-block-content image-zoom relative">
+                    <a href="blog-single.html" className=" ">
+                        <img className="blog-block-img " src="/blog7.png" alt="" />
+                        <div className="header-overlay hidden lg:flex absolute inset-x-0 bottom-0 justify-center">
+                            <h5 className="font-heading text-white text-xl p-12">5 Helpful Tips for Living Healthy Life</h5>
+                        </div>
+                    </a>
+                </figure>
+                <figure className="blog-block-content image-zoom relative">
+                    <a href="blog-single.html" className=" ">
+                        <img className="blog-block-img " src="/blog8.png" alt="" />
+                        <div className="header-overlay hidden lg:flex absolute inset-x-0 bottom-0 justify-center">
+                            <h5 className="font-heading text-white text-xl p-12">5 Helpful Tips for Living Healthy Life</h5>
+                        </div>
+                    </a>
+                </figure>
+                <figure className="blog-block-content image-zoom relative">
+                    <a href="blog-single.html" className=" ">
+                        <img className="blog-block-img " src="/blog9.png" alt="" />
+                        <div className="header-overlay hidden lg:flex absolute inset-x-0 bottom-0 justify-center">
+                            <h5 className="font-heading text-white text-xl p-12">5 Helpful Tips for Living Healthy Life</h5>
+                        </div>
+                    </a>
+                </figure>
+                <figure className="blog-block-content image-zoom relative">
+                    <a href="blog-single.html" className=" ">
+                        <img className="blog-block-img " src="/blog10.png" alt="" />
+                        <div className="header-overlay hidden lg:flex absolute inset-x-0 bottom-0 justify-center">
+                            <h5 className="font-heading text-white text-xl p-12">5 Helpful Tips for Living Healthy Life</h5>
+                        </div>
+                    </a>
+                </figure>
+                <figure className="blog-block-content image-zoom relative">
+                    <a href="blog-single.html" className=" ">
+                        <img className="blog-block-img " src="/blog11.png" alt="" />
+                        <div className="header-overlay hidden lg:flex absolute inset-x-0 bottom-0 justify-center">
+                            <h5 className="font-heading text-white text-xl p-12">5 Helpful Tips for Living Healthy Life</h5>
+                        </div>
+                    </a>
+                </figure>
+            </div>
+        </div>
+    )
+}
+
+export default Categories
--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from './components/Hero'
 import Cards from './components/Cards'
 import Subscription from './components/Subscription'
+import Categories from './components/Categories'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Cards />
+      <Categories />
       <Subscription />
     </div>
   )
