324ee7b9f8a3576a176522498993305831d1a492
Homayoun <homayoun763@gmail.com>
2024-04-06 02:27:21 +0330

    Cards

---
+++ app/[locale]/components/Cards.jsx
@@ -0,0 +1,152 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div>
+        <div className="container mx-auto py-28">
+      <div className="columns-1 md:columns-2 lg:columns-3 gap-7 px-5 lg:px-0">
+        <div className="blog-post py-10">
+
+          <div className="image-zoom ">
+            <a href="blog-single.html" className="blog-img"><img src="/blog6.png" alt="" className=""/></a>
+          </div>
+          <div className="pt-8">
+            <span className="blog-date uppercase">in <b>Travel Tips</b> on 12th Jan 2023</span>
+          </div>
+          <div className="">
+            <h3 className="py-5"><a href="blog-single.html" className="font-heading font-thin text-2xl hover:text-gray-500">I am
+                alone, and feel the charm
+                of
+                existence
+                created for the bliss</a></h3>
+            <p className="pb-10">I am so happy, my dear friend, so absorbed in the exquisite sense of mere
+              tranquil existence, that I neglect my talents. I should be incapable of drawing since
+            </p>
+            <a href="blog-single.html" className="font-heading text-sm font-normal py-4 px-8 bg-transparent hover:bg-black text-black hover:text-white border-black border-2 hover:border-transparent rounded-full transition duration-700 ease-in-out">
+              Read More
+            </a>
+          </div>
+
+        </div>
+        <div className="blog-post py-10">
+
+          <div className="image-zoom">
+            <a href="blog-single.html" className="blog-img"><img src="/blog5.png" alt="" className="img-fluid"/></a>
+          </div>
+          <div className="pt-8">
+            <span className="blog-date uppercase">in <b>Travel Tips</b> on 12th Jan 2023</span>
+          </div>
+          <div className="">
+            <h3 className="py-5"><a href="blog-single.html" className="font-heading font-thin text-2xl hover:text-gray-500">I am
+                alone, and feel the charm
+                of
+                existence
+                created for the bliss</a></h3>
+            <p className="pb-10">I am so happy, my dear friend, so absorbed in the exquisite sense of mere
+              tranquil existence, that I neglect my talents. I should be incapable of drawing since
+            </p>
+            <a href="blog-single.html" className="font-heading text-sm font-normal py-4 px-8 bg-transparent hover:bg-black text-black hover:text-white border-black border-2 hover:border-transparent rounded-full transition duration-700 ease-in-out">
+              Read More
+            </a>
+          </div>
+
+        </div>
+        <div className="blog-post py-10">
+
+          <div className="image-zoom">
+            <a href="blog-single.html" className="blog-img"><img src="/blog4.png" alt="" className="img-fluid"/></a>
+          </div>
+          <div className="pt-8">
+            <span className="blog-date uppercase">in <b>Travel Tips</b> on 12th Jan 2023</span>
+          </div>
+          <div className="">
+            <h3 className="py-5"><a href="blog-single.html" className="font-heading font-thin text-2xl hover:text-gray-500">I am
+                alone, and feel the charm
+                of
+                existence
+                created for the bliss</a></h3>
+            <p className="pb-10">I am so happy, my dear friend, so absorbed in the exquisite sense of mere
+              tranquil existence, that I neglect my talents. I should be incapable of drawing since
+            </p>
+            <a href="blog-single.html" className="font-heading text-sm font-normal py-4 px-8 bg-transparent hover:bg-black text-black hover:text-white border-black border-2 hover:border-transparent rounded-full transition duration-700 ease-in-out">
+              Read More
+            </a>
+          </div>
+
+        </div>
+        <div className="blog-post py-10">
+
+          <div className="image-zoom">
+            <a href="blog-single.html" className="blog-img"><img src="/blog3.png" alt="" className="img-fluid"/></a>
+          </div>
+          <div className="pt-8">
+            <span className="blog-date uppercase">in <b>Travel Tips</b> on 12th Jan 2023</span>
+          </div>
+          <div className="">
+            <h3 className="py-5"><a href="blog-single.html" className="font-heading font-thin text-2xl hover:text-gray-500">I am
+                alone, and feel the charm
+                of
+                existence
+                created for the bliss</a></h3>
+            <p className="pb-10">I am so happy, my dear friend, so absorbed in the exquisite sense of mere
+              tranquil existence, that I neglect my talents. I should be incapable of drawing since
+            </p>
+            <a href="blog-single.html" className="font-heading text-sm font-normal py-4 px-8 bg-transparent hover:bg-black text-black hover:text-white border-black border-2 hover:border-transparent rounded-full transition duration-700 ease-in-out">
+              Read More
+            </a>
+          </div>
+
+        </div>
+        <div className="blog-post py-10">
+
+          <div className="image-zoom">
+            <a href="blog-single.html" className="blog-img"><img src="/blog2.png" alt="" className="img-fluid"/></a>
+          </div>
+          <div className="pt-8">
+            <span className="blog-date uppercase">in <b>Travel Tips</b> on 12th Jan 2023</span>
+          </div>
+          <div className="">
+            <h3 className="py-5"><a href="blog-single.html" className="font-heading font-thin text-2xl hover:text-gray-500">I am
+                alone, and feel the charm
+                of
+                existence
+                created for the bliss</a></h3>
+            <p className="pb-10">I am so happy, my dear friend, so absorbed in the exquisite sense of mere
+              tranquil existence, that I neglect my talents. I should be incapable of drawing since
+            </p>
+            <a href="blog-single.html" className="font-heading text-sm font-normal py-4 px-8 bg-transparent hover:bg-black text-black hover:text-white border-black border-2 hover:border-transparent rounded-full transition duration-700 ease-in-out">
+              Read More
+            </a>
+          </div>
+
+        </div>
+        <div className="blog-post py-10">
+
+          <div className="image-zoom">
+            <a href="blog-single.html" className="blog-img"><img src="/blog1.png" alt="" className="img-fluid"/></a>
+          </div>
+          <div className="pt-8">
+            <span className="blog-date uppercase">in <b>Travel Tips</b> on 12th Jan 2023</span>
+          </div>
+          <div className="">
+            <h3 className="py-5"><a href="blog-single.html" className="font-heading font-thin text-2xl hover:text-gray-500">I am
+                alone, and feel the charm
+                of
+                existence
+                created for the bliss</a></h3>
+            <p className="pb-10">I am so happy, my dear friend, so absorbed in the exquisite sense of mere
+              tranquil existence, that I neglect my talents. I should be incapable of drawing since
+            </p>
+            <a href="blog-single.html" className="font-heading text-sm font-normal py-4 px-8 bg-transparent hover:bg-black text-black hover:text-white border-black border-2 hover:border-transparent rounded-full transition duration-700 ease-in-out">
+              Read More
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Cards
--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import Cards from './components/Cards'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Cards />
     </div>
   )
 }
